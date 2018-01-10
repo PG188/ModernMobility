23547bf2ad0499aadac7e59a3dd1f20db76bb84d
Nathan Fujimoto <fujimonj@mcmaster.ca>
2018-01-10 14:07:01 -0500

    actual updated MotorPID.ino

--- LowLevel/MotorPID/MotorPID.ino
+++ LowLevel/MotorPID/MotorPID.ino
@@ -1,23 +1,23 @@
 #include <Arduino.h>
 #include <PID_v1.h>    //Library details: https://playground.arduino.cc/Code/PIDLibrary
 
+#define InputPin 0
+#define SetPointPin 1
+#define OutputPin 3
+
 //Constants
 const double K_P = 1, 
-             K_I = 1,   //K values must be >= 0
-             K_D = 1,
-             OUT_MIN = 0,    //-99 (+ 99)
-             OUT_MAX = 198;  //+99 (+ 99)
+             K_I = 0.25,   //K values must be >= 0
+             K_D = 0,
+             OUT_MIN = 0,    //-99 (+ 99) 0
+             OUT_MAX = 99;  //+99 (+ 99) 198
 
-const int SAMPLE_TIME = 200; 
+const int SAMPLE_TIME = 20; 
 
 //Variables
-double Input = 0,    //The variable we are trying to control
-       Output = 0,   //The variable that will be adjusted by the PID
-       SetPoint = 0; //The value we are trying to get to or maintain
-
-float currentVel = 0, 
-      targetVel = 0; 
-      
+double Input,      //The variable we are trying to control
+       SetPoint,   //The value we are trying to get to or maintain
+       Output;     //The variable that will be adjusted by the PID
 
 //PID Object
 //PID(&double, &double, &double, double, double, double, Direction)
@@ -26,16 +26,26 @@ PID MotorPID(&Input, &Output, &SetPoint, K_P, K_I, K_D, DIRECT);
 /**************************************************************/
 
 void setup(){
+    Serial.begin(9600);
     initPID();
+    Input = 10; //Testing purposes 
 }
 
 void loop(){
-    currentVel = 0; //should = analogRead(#); 
-    targetVel = 0;  //should = analogRead(#); 
-    
-    doPID(currentVel, targetVel);
-
-    //analogWrite(#, Output);
+  //Input = analogRead(InputPin);
+  SetPoint = 20;  //SetPoint = analogRead(SetPointPin); 
+  doPID();
+  Serial.println(Input);
+  Serial.print(" ");
+  //Serial.println(SetPoint);
+  // For testing without motors:
+  if (Input < SetPoint) {
+    Input = Input + Output*2.0/99.0;
+  }
+  else if (Input > SetPoint) {
+    Input = Input - Output*2.0/99.0;
+  }
+  //analogWrite(#, Output);
 }
 
 /**************************************************************/
@@ -46,17 +56,16 @@ void initPID(){
     MotorPID.SetSampleTime(SAMPLE_TIME);        //By default this is 200ms
 }
 
-float doPID(float input, float setpoint){
-    Input = input;
-    SetPoint = setpoint;
-
+void doPID(){
     /*
      * MotorPID.Compute() computes the PID and returns true, 
      * if it does not compute anything it will return false
      */
-    if (!MotorPID.Compute())  
-    {
-        //<EXECUTE EMERGENCY ACTION>
+    if (!MotorPID.Compute()) {
+        //Serial.println("PID returned false");
+    }
+    else {
+      //Serial.print("Output Velocity = "); Serial.println(Output);
     }
 }
 
