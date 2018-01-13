5508046a5a0d0b4f9763d8415c001626a8d8cbc9
Nathan Fujimoto <fujimonj@mcmaster.ca>
2018-01-12 21:41:42 -0500

    Implemented serial communication between the arduino and the Pi. Configured nodes for it. Encountered inconsistency

--- ROS/python_node/serialNode.py
+++ ROS/python_node/serialNode.py
@@ -5,20 +5,25 @@ import serial
 
 from std_msgs.msg import ByteMultiArray
 
-def callback(data):
-	data	
+def callback(msg):
+	ser.write(msg)	#writing subscribed serial bytemultiarray
 
 def serialNode():
 	ser = serial.Serial('/dev/ttyNAME', 19200)
-	pub = rospy.Publisher('USB_out', ByteMultiArray, queue_size = 1000)
+	readSerial = bytearray()
+	
+	#Define Publisher
+	pub = rospy.Publisher('USB_toROS', ByteMultiArray, queue_size = 1000)
+	
 	rospy.init_node('serialNode', anonymous=True)
-	rospy.Subscriber("USB_in", ByteMultiArray, callback)
+
+	#Define Subscriber
+	rospy.Subscriber("USB_toArd", ByteMultiArray, callback)
 	rate = rospy.Rate(10)
 	while not rospy.is_shutdown():
-		//publishing bytemultiarray
-		pub.publish()
 		if ser.in_waiting > 12
-			ser.read(12)
+			readSerial = ser.read(12)
+			pub.publish(readSerial)		#publishing serial bytemultiarray
 
 if __name__ == 	'__main__'	
 	try:
