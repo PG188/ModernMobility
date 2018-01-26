ab925fd8301df8d65b27cf84fad68434ef7ec3bd
Aaron <asgjass@gmail.com>
2018-01-25 23:46:32 -0500

    added bluetooth node to HMI package, created HMI node, edited left/right velocity selector nodes

--- ROS/main_src/motor_control_right/src/rightVelocitySelection.cpp
+++ ROS/main_src/motor_control_right/src/rightVelocitySelection.cpp
@@ -2,19 +2,18 @@
 #include "std_msgs/Float32.h"
 #include "std_msgs/UInt16.h"
 
-#define manual 0
-#define autonomous 1
-#define free_roll 0
-#define assisted 1
+#define manual_free_roll 0
+#define manual_assisted 1
+#define auto_go_user 2
+#define auto_go_park 3
+
 #define free_roll_flag 999.0
 #define max_slider_vel 1
 
 std_msgs::Float32 vel_cmd_in;
 std_msgs::UInt16 sliderVal_in;
 std_msgs::Float32 vel_cmd_out;
-
-int manual_mode = assisted; //Change to topic later
-int walker_mode = manual; //Change to topic later
+std_msgs::UInt16 walker_mode;
 
 void velocity_cmd_callback(const std_msgs::Float32::ConstPtr& VelCommand) {
 	vel_cmd_in.data = VelCommand->data;
@@ -22,6 +21,10 @@ void velocity_cmd_callback(const std_msgs::Float32::ConstPtr& VelCommand) {
 
 void slider_callback(const std_msgs::UInt16::ConstPtr& sliderVal) {
 	sliderVal_in.data = sliderVal->data;
+}
+
+void mode_callback(const std_msgs::UInt16::ConstPtr& modeVal) {
+	walker_mode.data = modeVal->data;
 }
 
 int main(int argc, char** argv){
@@ -38,6 +41,8 @@ int main(int argc, char** argv){
 	ros::Subscriber rmtr_velocity_sub = nh.subscribe("rwheel_vtarget", 1, velocity_cmd_callback);
 	//Adds subscriber that subscribesleft slider value on appropriate topic, reads with callback
 	ros::Subscriber rslider_sub = nh.subscribe("rslider_pos", 1, slider_callback);
+	//Adds subscriber that subscribes to walker mode on appropriate topic, reads with callback
+	ros::Subscriber walker_mode_sub = nh.subscribe("walker_mode", 1, mode_callback);
 
 	//Publisher initialization
 	//Adds publisher that publishes left motor velocity on appropriate topic, with a buffer size of 1000	
@@ -47,7 +52,7 @@ int main(int argc, char** argv){
 
 	while(ros::ok()){
 		ros::spinOnce(); //Allows the subscriber callbacks to execute if new data available
-		if (walker_mode == manual) {
+		/*if (walker_mode == manual) {
 			if (manual_mode == free_roll) {
 				vel_cmd_out.data = free_roll_flag; //Identifies free roll to motor controller
 			} else if (manual_mode == assisted) {
@@ -60,6 +65,19 @@ int main(int argc, char** argv){
 				}
 			}
 		} else if (walker_mode == autonomous) {
+			vel_cmd_out = vel_cmd_in;
+		}*/
+		if (walker_mode.data == manual_free_roll) {
+			vel_cmd_out.data = free_roll_flag; //Identifies free roll to motor controller
+		} else if (walker_mode.data == manual_assisted) {
+			if (sliderVal_in.data <= 407) {
+				vel_cmd_out.data = max_slider_vel*(-1 + float(sliderVal_in.data)/407);
+			} else if (sliderVal_in.data >= 408 && sliderVal_in.data <= 615) {
+				vel_cmd_out.data = 0; //Identifies free roll to motor controller
+			} else {
+				vel_cmd_out.data = max_slider_vel*(float(sliderVal_in.data) - 616)/407;
+			}
+		} else if (walker_mode.data == auto_go_user || walker_mode.data == auto_go_park) {
 			vel_cmd_out = vel_cmd_in;
 		}
 		rmtr_velocity_pub.publish(vel_cmd_out);
