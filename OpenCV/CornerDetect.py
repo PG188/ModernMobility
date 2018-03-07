d08d25176f6a1eaaa40e590b481d989deee89f8b
Aaron <asgjass@gmail.com>
2018-03-07 15:23:08 -0500

    added location logic to cornedetect script

--- OpenCV/CornerDetect.py
+++ OpenCV/CornerDetect.py
@@ -7,43 +7,80 @@ font = cv2.FONT_HERSHEY_SIMPLEX #font type
 
 while (cv2.waitKey(1) & 0xFF) != ord('q'):
     try:
-        ret, frame = cap.read() #ret gets True or false (if an image is being returned), frame gets the image
-        #frame  = cv2.fastNlMeansDenoisingColored(frame,None,10,10,7,21)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  #converts to grayscale
-        gray  = cv2.fastNlMeansDenoising(gray,None,50,7,21)
-        ret, mask = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY_INV)    #If pixel value is above 220 it will convert to 255(white), below will turn to black (because binary)
-        gray = cv2.bitwise_not(mask)
+        ret, inFrame = cap.read() #ret gets True or false (if an image is being returned), frame gets the image
+        outFrame = cv2.cvtColor(inFrame, cv2.COLOR_BGR2GRAY)  #converts to grayscale
+        outFrame  = cv2.fastNlMeansDenoising(outFrame,None,50,7,21) #filters out noise (frame, None, higher filter out more noise but gives less detail, filter parameter, filtaer parameter)
+        ret, mask = cv2.threshold(outFrame, 100, 255, cv2.THRESH_BINARY_INV)    #If pixel value is above 220 it will convert to 255(white), below will turn to black (because binary)
+        outFrame = cv2.bitwise_not(mask)
+        outframe = cv2.Canny(outFrame, 50, 50)
 
-        #cv2.imshow('gray', gray)
-
-        gray = cv2.Canny(gray, 50, 50)
-
-        corners = cv2.goodFeaturesToTrack(gray, 100, 0.01, 60)  #(image, up to 100 coerners, quality, minimum distance between corners)
+        corners = cv2.goodFeaturesToTrack(outframe, 100, 0.01, 60)  #(image, up to 100 coerners, quality, minimum distance between corners)
         #print(corners)
         if not(corners is None):
             
             corners = np.int0(corners)
-            #print(corners)
+
             numCorners = 0
+            avX = 0
+            avY = 0
 
             for corner in corners:
                 x, y = corner.ravel()
-                #print('Corner: ' + str(corner) + '\tRavel: ' + str(corner.ravel()))
-                cv2.circle(frame, (x,y), 5, (0,0,255) , 1)
+                cv2.circle(inFrame, (x,y), 5, (255,0,0) , 1)
+                avX = avX + x
+                avY = avY + y
                 numCorners += 1
 
-            if numCorners <= 24:
+            rows, cols, channels = inFrame.shape  #get how many rows and columns in the frams being returned
+            Tbound = 0.4*rows
+            Bbound = 0.6*rows
+            Lbound = 0.4*cols
+            Rbound = 0.6*cols
+
+            avX = avX/numCorners
+            avY = avY/numCorners
+
+            if numCorners <= 2:
                 pass
-            elif numCorners <= 48:
-                cv2.putText(frame, 'Location: 1', (0,440), font, 0.5, (0,255,0), 1, cv2.LINE_AA) #(image, label, start text at, font, size of font, colour(BGR), line width, Anti-aliasing)
-            elif numCorners <= 72:
-                cv2.putText(frame, 'Location: 2', (0,440), font, 0.5, (0,255,0), 1, cv2.LINE_AA) #(image, label, start text at, font, size of font, colour(BGR), line width, Anti-aliasing)
+            
+            elif numCorners <= 6:
+
+                if (avX > Lbound) and (avX < Rbound) and (avY > Tbound) and (avY < Bbound):
+                    cv2.putText(inFrame, 'Location: 1 Centred', (0,440), font, 1, (0,255,0), 1, cv2.LINE_AA) #(image, label, start text at, font, size of font, colour(BGR), line width, Anti-aliasing)
+                else:
+                    if (avY >= Bbound):
+                        cv2.putText(inFrame, 'Location: 1 is North', (0,440), font, 1, (0,0,255), 1, cv2.LINE_AA) #(image, label, start text at, font, size of font, colour(BGR), line width, Anti-aliasing)
+                    
+                    elif (avX <= Lbound):
+                        cv2.putText(inFrame, 'Location: 1 is East', (0,440), font, 1, (0,0,255), 1, cv2.LINE_AA) #(image, label, start text at, font, size of font, colour(BGR), line width, Anti-aliasing)
+                        
+                    elif (avX >=Rbound):
+                        cv2.putText(inFrame, 'Location: 1 is West', (0,440), font, 1, (0,0,255), 1, cv2.LINE_AA) #(image, label, start text at, font, size of font, colour(BGR), line width, Anti-aliasing)
+
+                    else:
+                        cv2.putText(inFrame, 'Location: 1 is South', (0,440), font, 1, (0,0,255), 1, cv2.LINE_AA) #(image, label, start text at, font, size of font, colour(BGR), line width, Anti-aliasing)
+                
+            elif numCorners <= 10:
+                if (avX > Lbound) and (avX < Rbound) and (avY > Tbound) and (avY < Bbound):
+                    cv2.putText(inFrame, 'Location: 1 Centred', (0,440), font, 1, (0,255,0), 1, cv2.LINE_AA) #(image, label, start text at, font, size of font, colour(BGR), line width, Anti-aliasing)
+                else:
+                    if (avY >= Bbound):
+                        cv2.putText(inFrame, 'Location: 1 is North', (0,440), font, 1, (0,0,255), 1, cv2.LINE_AA) #(image, label, start text at, font, size of font, colour(BGR), line width, Anti-aliasing)
+                    
+                    elif (avX <= Lbound):
+                        cv2.putText(inFrame, 'Location: 1 is East', (0,440), font, 1, (0,0,255), 1, cv2.LINE_AA) #(image, label, start text at, font, size of font, colour(BGR), line width, Anti-aliasing)
+                        
+                    elif (avX >=Rbound):
+                        cv2.putText(inFrame, 'Location: 1 is West', (0,440), font, 1, (0,0,255), 1, cv2.LINE_AA) #(image, label, start text at, font, size of font, colour(BGR), line width, Anti-aliasing)
+
+                    else:
+                        cv2.putText(inFrame, 'Location: 1 is South', (0,440), font, 1, (0,0,255), 1, cv2.LINE_AA) #(image, label, start text at, font, size of font, colour(BGR), line width, Anti-aliasing)
 
             C = str(numCorners)
-            cv2.putText(frame, 'Number of Corners: ' + C, (0,15), font, 0.5, (0,255,0), 1, cv2.LINE_AA) #(image, label, start text at, font, size of font, colour(BGR), line width, Anti-aliasing)
+            cv2.putText(inFrame, 'Number of Corners: ' + C, (0,15), font, 0.5, (255,0,0), 1, cv2.LINE_AA) #(image, label, start text at, font, size of font, colour(BGR), line width, Anti-aliasing)
             
-            #cv2.imshow('frame', frame)  #window for original image
-            #cv2.imshow('gray and Canny', gray)    #window for grayscale image
+            cv2.imshow('inFrame', inFrame)  #window for original image
+            cv2.imshow('outFrame', outFrame)    #window for grayscale image
 
         else:
             print('Corners is NONE!')
