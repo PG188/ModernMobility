e82f55b2261e6a364e3c4b76333c19f9adcc68aa
Tyler <jasstsg@gmail.com>
2018-03-08 18:37:58 -0500

    Pushing changes so aaron can test on his laptop with external webcam

--- OpenCV/ContourDetect.py
+++ OpenCV/ContourDetect.py
@@ -5,6 +5,7 @@ cap = cv2.VideoCapture(0)   #0 indicates first webcam in system
 #cap = cv2.VideoCapture(1)   #1 indicates second webcam in system
 
 font = cv2.FONT_HERSHEY_SIMPLEX #font type
+E_COEFF = 0.01   #Coefficient for epsilon value
 
 blue = (255,0,0)
 green = (0,255,0)
@@ -43,57 +44,70 @@ while (cv2.waitKey(1) & 0xFF) != ord('q'):
         outFrame = cv2.bitwise_not(mask)
         outFrame = cv2.Canny(outFrame, 50, 50)
 
-        corners = cv2.goodFeaturesToTrack(outFrame, 100, 0.01, 60)  #(image, up to 100 corners, quality, minimum distance between corners)
-    
-        if not(corners is None):
+        _,contours,_ = cv2.findContours(outFrame,1,2)
+        for cnt in contours:
+            approx = cv2.approxPolyDP(cnt,E_COEFF*cv2.arcLength(cnt,True),True)
+            length = len(approx)
+            #print "\nPoints found = ", length
+            if length == 12:
+                cv2.drawContours(inFrame, [cnt], 0, (0,0,255), -1)
+
+        cv2.imshow('inFrame', inFrame)
+        cv2.imshow('outFrame', outFrame)
+
+
             
-            corners = np.int0(corners)
-
-            numCorners = 0
-            avgX = 0
-            avgY = 0
-
-            for corner in corners:
-                x, y = corner.ravel()
-                cv2.circle(inFrame, (x,y), 5, blue , 1)
-                avgX += x
-                avgY += y
-                numCorners += 1
-
-            rows, cols, channels = inFrame.shape  #get how many rows and columns in the frams being returned
-            Tbound = 0.35*rows
-            Bbound = 0.65*rows
-            Lbound = 0.35*cols
-            Rbound = 0.65*cols
-
-            avgX /= numCorners
-            avgY /= numCorners
-
-            if numCorners <= 12:
-                pass
-            
-            elif numCorners <= 24:
-                locate('1')
-                
-            elif numCorners <= 36:
-                locate('2')
-
-            elif numCorners <= 48:
-                locate('3')
-
-            elif numCorners <= 60:
-                locate('4')
-
-            C = str(numCorners)
-            cv2.putText(inFrame, 'Number of Corners: ' + C, (0,25), font, 1, blue, 1, cv2.LINE_AA) #(image, label, start text at, font, size of font, colour(BGR), line width, Anti-aliasing)
-            
-            cv2.imshow('inFrame', inFrame)  #window for original image
-            cv2.imshow('outFrame', outFrame)
-
-        else:
-            print('Corners is NONE!')
-            cv2.imshow('inFrame', inFrame)  #window for original image
-            cv2.imshow('outFrame', outFrame)
+##        corners = cv2.goodFeaturesToTrack(outFrame, 100, 0.01, 60)  #(image, up to 100 corners, quality, minimum distance between corners)
+##
+##        if not(corners is None):
+##            
+##            corners = np.int0(corners)
+##
+##            numCorners = 0
+##            avgX = 0
+##            avgY = 0
+##
+##            for corner in corners:
+##                x, y = corner.ravel()
+##                cv2.circle(inFrame, (x,y), 5, blue , 1)
+##                avgX += x
+##                avgY += y
+##                numCorners += 1
+##
+##            rows, cols, channels = inFrame.shape  #get how many rows and columns in the frams being returned
+##            Tbound = 0.35*rows
+##            Bbound = 0.65*rows
+##            Lbound = 0.35*cols
+##            Rbound = 0.65*cols
+##
+##            avgX /= numCorners
+##            avgY /= numCorners
+##
+##            if numCorners <= 12:
+##                pass
+##            
+##            elif numCorners <= 24:
+##                locate('1')
+##                
+##            elif numCorners <= 36:
+##                locate('2')
+##
+##            elif numCorners <= 48:
+##                locate('3')
+##
+##            elif numCorners <= 60:
+##                locate('4')
+##
+##            C = str(numCorners)
+##            cv2.putText(inFrame, 'Number of Corners: ' + C, (0,25), font, 1, blue, 1, cv2.LINE_AA) #(image, label, start text at, font, size of font, colour(BGR), line width, Anti-aliasing)
+##            
+##            cv2.imshow('inFrame', inFrame)  #window for original image
+##            cv2.imshow('outFrame', outFrame)
+##
+##        else:
+##            print('Corners is NONE!')
+##            cv2.imshow('inFrame', inFrame)  #window for original image
+##            cv2.imshow('outFrame', outFrame)
 
     except Exception as e:
         print('ERROR: ' + str(e))
