ab353ce17f9b480abffcdee216821afb27c3a109
Aaron <asgjass@gmail.com>
2018-03-07 19:07:17 -0500

    more logic changes

--- OpenCV/CornerDetect.py
+++ OpenCV/CornerDetect.py
@@ -1,10 +1,46 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0)   #0 indicates first webcam in system
+#cap = cv2.VideoCapture(0)   #0 indicates first webcam in system
+cap = cv2.VideoCapture(1)   #1 indicates second webcam in system
 print('Video Capture initiated')
 font = cv2.FONT_HERSHEY_SIMPLEX #font type
 
+blue = (255,0,0)
+green = (0,255,0)
+red = (0,0,255)
+
+#================================================================================================
+
+def locate(l):
+    textLoc = (0,470)
+
+    if (avY >= Bbound):     #cases where target is north of walker
+        if (avX <= Lbound):     #target is East of walker
+            cv2.putText(inFrame, 'Location: '+ l + ' is NorthEast', textLoc, font, 1, red, 1, cv2.LINE_AA)
+        elif (avX >= Rbound):   #target is West of walker
+            cv2.putText(inFrame, 'Location: '+ l + ' is NorthWest', textLoc, font, 1, red, 1, cv2.LINE_AA)
+        else:                   #target is North of walker
+            cv2.putText(inFrame, 'Location: '+ l + ' is North', textLoc, font, 1, red, 1, cv2.LINE_AA)
+
+    elif (avY <= Bbound):   #cases where target is South of walker
+        if (avX <= Lbound):     #target is East of walker
+            cv2.putText(inFrame, 'Location: '+ l + ' is SouthEast', textLoc, font, 1, red, 1, cv2.LINE_AA)
+        elif (avX >= Rbound):   #target is West of walker
+            cv2.putText(inFrame, 'Location: '+ l + ' is SouthWest', textLoc, font, 1, red, 1, cv2.LINE_AA)
+        else:                   #target is South of walker
+            cv2.putText(inFrame, 'Location: '+ l + ' is South', textLoc, font, 1, red, 1, cv2.LINE_AA)
+
+    else:                   #cases where target in-line with the walker
+        if (avX <= Lbound):     #target is East of walker
+            cv2.putText(inFrame, 'Location: '+ l + ' is East', textLoc, font, 1, red, 1, cv2.LINE_AA)
+        elif (avX >= Rbound):   #target is West of walker
+            cv2.putText(inFrame, 'Location: '+ l + ' is West', textLoc, font, 1, red, 1, cv2.LINE_AA)
+        else:                   #target is centered
+            cv2.putText(inFrame, 'Location: '+ l + ' Centered!', textLoc, font, 1, green, 1, cv2.LINE_AA)
+        
+#================================================================================================
+            
 while (cv2.waitKey(1) & 0xFF) != ord('q'):
     try:
         ret, inFrame = cap.read() #ret gets True or false (if an image is being returned), frame gets the image
@@ -14,7 +50,7 @@ while (cv2.waitKey(1) & 0xFF) != ord('q'):
         outFrame = cv2.bitwise_not(mask)
         outframe = cv2.Canny(outFrame, 50, 50)
 
-        corners = cv2.goodFeaturesToTrack(outframe, 100, 0.01, 60)  #(image, up to 100 coerners, quality, minimum distance between corners)
+        corners = cv2.goodFeaturesToTrack(outframe, 100, 0.01, 60)  #(image, up to 100 corners, quality, minimum distance between corners)
         #print(corners)
         if not(corners is None):
             
@@ -26,16 +62,16 @@ while (cv2.waitKey(1) & 0xFF) != ord('q'):
 
             for corner in corners:
                 x, y = corner.ravel()
-                cv2.circle(inFrame, (x,y), 5, (255,0,0) , 1)
+                cv2.circle(inFrame, (x,y), 5, blue , 1)
                 avX = avX + x
                 avY = avY + y
                 numCorners += 1
 
             rows, cols, channels = inFrame.shape  #get how many rows and columns in the frams being returned
-            Tbound = 0.4*rows
-            Bbound = 0.6*rows
-            Lbound = 0.4*cols
-            Rbound = 0.6*cols
+            Tbound = 0.35*rows
+            Bbound = 0.65*rows
+            Lbound = 0.35*cols
+            Rbound = 0.65*cols
 
             avX = avX/numCorners
             avY = avY/numCorners
@@ -43,55 +79,34 @@ while (cv2.waitKey(1) & 0xFF) != ord('q'):
             if numCorners <= 2:
                 pass
             
-            elif numCorners <= 6:
-
-                if (avX > Lbound) and (avX < Rbound) and (avY > Tbound) and (avY < Bbound):
-                    cv2.putText(inFrame, 'Location: 1 Centred', (0,440), font, 1, (0,255,0), 1, cv2.LINE_AA) #(image, label, start text at, font, size of font, colour(BGR), line width, Anti-aliasing)
-                else:
-                    if (avY >= Bbound):
-                        cv2.putText(inFrame, 'Location: 1 is North', (0,440), font, 1, (0,0,255), 1, cv2.LINE_AA) #(image, label, start text at, font, size of font, colour(BGR), line width, Anti-aliasing)
-                    
-                    elif (avX <= Lbound):
-                        cv2.putText(inFrame, 'Location: 1 is East', (0,440), font, 1, (0,0,255), 1, cv2.LINE_AA) #(image, label, start text at, font, size of font, colour(BGR), line width, Anti-aliasing)
-                        
-                    elif (avX >=Rbound):
-                        cv2.putText(inFrame, 'Location: 1 is West', (0,440), font, 1, (0,0,255), 1, cv2.LINE_AA) #(image, label, start text at, font, size of font, colour(BGR), line width, Anti-aliasing)
-
-                    else:
-                        cv2.putText(inFrame, 'Location: 1 is South', (0,440), font, 1, (0,0,255), 1, cv2.LINE_AA) #(image, label, start text at, font, size of font, colour(BGR), line width, Anti-aliasing)
-                
             elif numCorners <= 10:
-                if (avX > Lbound) and (avX < Rbound) and (avY > Tbound) and (avY < Bbound):
-                    cv2.putText(inFrame, 'Location: 1 Centred', (0,440), font, 1, (0,255,0), 1, cv2.LINE_AA) #(image, label, start text at, font, size of font, colour(BGR), line width, Anti-aliasing)
-                else:
-                    if (avY >= Bbound):
-                        cv2.putText(inFrame, 'Location: 1 is North', (0,440), font, 1, (0,0,255), 1, cv2.LINE_AA) #(image, label, start text at, font, size of font, colour(BGR), line width, Anti-aliasing)
-                    
-                    elif (avX <= Lbound):
-                        cv2.putText(inFrame, 'Location: 1 is East', (0,440), font, 1, (0,0,255), 1, cv2.LINE_AA) #(image, label, start text at, font, size of font, colour(BGR), line width, Anti-aliasing)
-                        
-                    elif (avX >=Rbound):
-                        cv2.putText(inFrame, 'Location: 1 is West', (0,440), font, 1, (0,0,255), 1, cv2.LINE_AA) #(image, label, start text at, font, size of font, colour(BGR), line width, Anti-aliasing)
+                locate('1')
+                
+            elif numCorners <= 18:
+                locate('2')
 
-                    else:
-                        cv2.putText(inFrame, 'Location: 1 is South', (0,440), font, 1, (0,0,255), 1, cv2.LINE_AA) #(image, label, start text at, font, size of font, colour(BGR), line width, Anti-aliasing)
+            elif numCorners <= 26:
+                locate('3')
+
+            elif numCorners <= 32:
+                locate('4')
 
             C = str(numCorners)
-            cv2.putText(inFrame, 'Number of Corners: ' + C, (0,15), font, 0.5, (255,0,0), 1, cv2.LINE_AA) #(image, label, start text at, font, size of font, colour(BGR), line width, Anti-aliasing)
+            cv2.putText(inFrame, 'Number of Corners: ' + C, (0,25), font, 1, blue, 1, cv2.LINE_AA) #(image, label, start text at, font, size of font, colour(BGR), line width, Anti-aliasing)
             
             cv2.imshow('inFrame', inFrame)  #window for original image
-            cv2.imshow('outFrame', outFrame)    #window for grayscale image
+            cv2.imshow('outFrame', outFrame)
 
         else:
             print('Corners is NONE!')
-            #cv2.imshow('frame', frame)  #window for original image
-            
+            cv2.imshow('inFrame', inFrame)  #window for original image
+            cv2.imshow('outFrame', outFrame)
 
     except Exception as e:
         print('ERROR: ' + str(e))
-        
-
 
 cap.release()   #you gotta release
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
