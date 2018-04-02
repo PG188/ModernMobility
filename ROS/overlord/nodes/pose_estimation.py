8078203ad76094a41305bd66e488cd961624b6d5
Aaron <asgjass@gmail.com>
2018-04-02 16:18:43 -0400

    Added function for integration between ROS and reading values from Tyler's map

--- ROS/overlord/nodes/pose_estimation.py
+++ ROS/overlord/nodes/pose_estimation.py
@@ -24,105 +24,9 @@ from cv2 import aruco
 import numpy as np
 import locWalker
 import Mag3D
+import ReadMap
 
-def TestImage():
-    
-    #Initialization (should not have to do every time)
-    marker_length = 0.165 #Any unit. Pose estimation will have the same unit
-    
-    aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
-
-    webcam_cals = np.load('webcam_cals.npz')
-    camera_matrix = webcam_cals['camera']
-    dist_coeffs = webcam_cals['dist']
-
-    NUM_MARKERS = 5 #You are allowed to change this (breaks if you go higher than 20 with size 40 atm)
-    ARUCO_SIZE = 40 #You are allowed to change this
-    WIN_WIDTH = 480
-    WIN_LENGTH = 640
-    SPACING = 5
-
-    ids = []
-
-    blank = np.ones((WIN_WIDTH,WIN_LENGTH),np.uint8)*255
-
-    i = 0
-    while i < NUM_MARKERS:
-        ids.append(cv2.aruco.drawMarker(aruco_dict, i, ARUCO_SIZE, 1))
-        xstart = i*(ARUCO_SIZE + SPACING) + SPACING
-        ystart = i*(ARUCO_SIZE + SPACING) + SPACING
-        
-        if xstart > (WIN_WIDTH - ARUCO_SIZE):
-            xstart -= (WIN_WIDTH - ARUCO_SIZE)
-
-        if ystart > (WIN_LENGTH - ARUCO_SIZE):
-            ystart -= (WIN_LENGTH - ARUCO_SIZE)
-        
-        xend = xstart + ARUCO_SIZE
-        
-        yend = ystart + ARUCO_SIZE
-        
-        blank[xstart:xend,ystart:yend] = ids[i]
-        
-        i += 1
-        
-    corners, ids, rejected = aruco.detectMarkers(blank, aruco_dict)
-
-    aruco.drawDetectedMarkers(blank, corners, ids, (120,120,120))
-
-    i = 0
-    cm = None #Closest Marker to walker
-    mm = None #Marker magnitude
-    cmm = None #Comparing marker magnitude
-    dx = None #distance vector between walker and marker in on x-axis (Scalar value)
-    dy = None #distance vector between walker and marker in on y-axis (Scalar value)
-    yaw = None #angle between Aruco's "North" and Walker's "North"
-
-    if not(ids is None): #Markers were detected
-        print('\nTestImage:\tMarkers detected')
-        rvec, tvec, _ = aruco.estimatePoseSingleMarkers(corners, marker_length, camera_matrix, dist_coeffs)
-
-        while i < len(ids):  #This loop determines which marker is the closest
-            
-            if i == 0:
-
-                mm = Mag3D.Mag3D(tvec[i][0][0], tvec[i][0][1],tvec[i][0][2])
-                tmp = mm
-                arucoID = ids[i][0]
-                dx = tvec[i][0][0]
-                dy = tvec[i][0][1]
-                yaw = rvec[i][0][0]
-                
-            else:
-                tmp = Mag3D.Mag3D(tvec[i][0][0], tvec[i][0][1],tvec[i][0][2])
-
-                if tmp < mm:
-                    mm = tmp
-                    arucoID = ids[i][0]
-                    dx = tvec[i][0][0]
-                    dy = tvec[i][0][1]
-                    yaw = rvec[i][0][0]
-                
-            print ('\nTestImage:\ti = %d:\t Magnitude = %f, arucoID = %d, rvec[%d] = %s\ttvec[%d] = %s' %(i, tmp, ids[i][0], i, rvec[i], i, tvec[i]))
-            aruco.drawAxis(blank, camera_matrix, dist_coeffs, rvec[i], tvec[i], marker_length)
-            i += 1
-        
-    else:
-        print('\nTestImage:\tNo markers detected\n')
-
-    print('\nTestImage:\tFinal Values:\tMagnitude = %f, arucoID = %d, dx = %f, dy = %f, yaw = %f' %(mm, arucoID, dx, dy, yaw))
-
-    cv2.imshow('blank', blank)
-
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-    xWalker, yWalker = locWalker.locWalker(arucoID, dx, dy)
-
-    
-    return xWalker, yWalker, yaw
-
-def TestVideo():
+def marker_detect():
     
     marker_length = 0.165 #Any unit. Pose estimation will have the same unit
     
@@ -192,7 +96,10 @@ def TestVideo():
     xWalker, yWalker = locWalker.locWalker(arucoID, dx, dy)    
     return xWalker, yWalker, yaw
 
-#TestImage()
-#TestVideo()
+def get_pose(location):
+    if location == 'walker':
+        return marker_detect()
+    else:
+        return ReadMap.getConstPose(location)
     
 
