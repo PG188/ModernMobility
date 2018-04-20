53dde82aa6d4effe5bba604654dfde59167062e6
Aaron <asgjass@gmail.com>
2018-04-19 23:30:11 -0400

    Phone and Touchscreen commands are successfully being forwarded to client on the Pi, still need to test this senario by moving the client onto a computer

--- TouchscreenCode/Client.py
+++ TouchscreenCode/Client.py
@@ -1,22 +1,24 @@
 import socket
 
 #TCP_IP = '169.254.10.85'
-TCP_IP = '172.17.50.37' 
+TCP_IP = '127.0.0.1' 
 TCP_PORT = 8080
 BUFFER_SIZE = 1024
+MESSAGE = '33'
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
+print('Client.py successfully connected')
 
-#s.send(bytes(MESSAGE, 'utf-8')))
+s.send(bytes(MESSAGE, 'utf-8'))
 #s.send(MESSAGE)
 while 1:
     data = s.recv(BUFFER_SIZE)
-    if not data: break
+    if not data:
+        print('Disconnected!')
+        break
     print ("Client.py: Received ", data)
+    #determine what cmd is being requested
     
 s.close()
-
-
-
