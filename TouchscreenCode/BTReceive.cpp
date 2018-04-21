91197759e5fa49b04817244fb94b2069f83d06fe
Aaron <asgjass@gmail.com>
2018-04-21 01:23:08 -0400

    Renaming and organizing files

--- TouchscreenCode/BTReceive.cpp
+++ TouchscreenCode/BTReceive.cpp
@@ -8,6 +8,149 @@
 #include <iostream>
 #include <math.h>
 
+//=====================================
+/**
+    C++ client example using sockets
+*/
+#include<iostream>    //cout
+//#include<stdio.h> //printf
+#include<string.h>    //strlen
+#include<string>  //string
+//#include<sys/socket.h>    //socket
+#include<arpa/inet.h> //inet_addr
+#include<netdb.h> //hostent
+
+using namespace std;
+ 
+/**
+    TCP Client class
+*/
+class tcp_client
+{
+private:
+    int sock;
+    std::string address;
+    int port;
+    struct sockaddr_in server;
+     
+public:
+    tcp_client();
+    bool conn(string, int);
+    bool send_data(string data);
+    string receive(int);
+};
+ 
+tcp_client::tcp_client()
+{
+    sock = -1;
+    port = 8080;
+    address = "127.0.0.1";
+}
+
+/**
+    Connect to a host on a certain port number
+*/
+bool tcp_client::conn(string address , int port)
+{
+    //create socket if it is not already created
+    if(sock == -1)
+    {
+        //Create socket
+        sock = socket(AF_INET , SOCK_STREAM , 0);
+        if (sock == -1)
+        {
+            perror("\nBTReceive: ERROR: Could not create socket\n");
+        }
+         
+        cout<<"BTReceive: TCP Socket created\n";
+    }
+    else    {   /* OK , nothing */  }
+     
+    //setup address structure
+    if(inet_addr(address.c_str()) == -1)
+    {
+        struct hostent *he;
+        struct in_addr **addr_list;
+         
+        //resolve the hostname, its not an ip address
+        if ( (he = gethostbyname( address.c_str() ) ) == NULL)
+        {
+            //gethostbyname failed
+            herror("BTReceive: gethostbyname");
+            cout<<"\nBTReceive: ERROR: Failed to resolve hostname\n";
+             
+            return false;
+        }
+         
+        //Cast the h_addr_list to in_addr , since h_addr_list also has the ip address in long format only
+        addr_list = (struct in_addr **) he->h_addr_list;
+ 
+        for(int i = 0; addr_list[i] != NULL; i++)
+        {
+            //strcpy(ip , inet_ntoa(*addr_list[i]) );
+            server.sin_addr = *addr_list[i];
+             
+            cout<<address<<" resolved to "<<inet_ntoa(*addr_list[i])<<endl;
+             
+            break;
+        }
+    }
+     
+    //plain ip address
+    else
+    {
+        server.sin_addr.s_addr = inet_addr( address.c_str() );
+    }
+     
+    server.sin_family = AF_INET;
+    server.sin_port = htons( port );
+     
+    //Connect to remote server
+    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        perror("BTReceive: connect failed. Error");
+        return 1;
+    }
+     
+    cout<<"BTReceive: Connected\n";
+    return true;
+}
+ 
+/**
+    Send data to the connected host
+*/
+bool tcp_client::send_data(string data)
+{
+    //Send some data
+    if( send(sock , data.c_str() , strlen( data.c_str() ) , 0) < 0)
+    {
+        perror("BTReceive: Send failed : ");
+        return false;
+    }
+    cout<<"BTReceive: Data sent from phone\n\n";
+     
+    return true;
+}
+ 
+/**
+    Receive data from the connected host
+*/
+string tcp_client::receive(int size=512)
+{
+    char buffer[size];
+    string reply;
+     
+    //Receive a reply from the server
+    if( recv(sock , buffer , sizeof(buffer) , 0) < 0)
+    {
+        puts("BTReceive: recv failed");
+    }
+     
+    reply = buffer;
+    return reply;
+}
+
+//=====================================
 
 float S, E, M, num, x, y, o;
 
@@ -22,7 +165,7 @@ float toFloatNum(char b0, char b1, char b2, char b3){
 	return num;
 }
 
-int interpretCmd(char *buf){
+int interpretCmd(char *buf, tcp_client &c){
 	
 	int cmd = -1;
 
@@ -31,47 +174,53 @@ int interpretCmd(char *buf){
 	switch(data){
 	
 		case (char)0:
-			printf("Command Cancelled\n");
+			printf("BTReceive: Command Cancelled\n");
 			//call function that sends cmd to master controller
+			c.send_data("2");
 			cmd = 0;
 			
 			return 0;
 		break;
 		
 		case (char)1:
-			printf("SmartWalker is heading to docking station\n");
+			printf("BTReceive: SmartWalker is heading to docking station\n");
 			//call function that sends cmd to master controller
+			c.send_data("3");
 			cmd = 1;
 
 			return 0;
 		break;
 		
 		case (char)2:
-			printf("SmartWalker is parking\n");
+			printf("BTReceive: SmartWalker is parking\n");
 			//call function that sends cmd to master controller
+			c.send_data("4");
 			cmd = 2;
 			
 			return 0;
 		break;
 		
 		case (char)3:
-			printf("Extra command\n");
+			printf("BTReceive: Extra command\n");
 			//call function that sends cmd to master controller
+			c.send_data("5");
 			cmd = 3;
 			
 			return 0;
 		break;
 		
 		case (char)4:
-			printf("Extra command 2\n");
+			printf("BTReceive: Extra command 2\n");
 			//call function that sends cmd to master controller
+			c.send_data("6");
 			cmd = 4;
 			
 			return 0;
 		break;
 		
 		case (char)9:
-			printf("Disconnecting...");
+			printf("BTReceive: Disconnecting...");
+			c.send_data("-1");
 			return 1;
 	}
 }
@@ -92,6 +241,30 @@ int interpretPose(char *buf){
 
 int main(int argc, char **argv){
 
+	//===TCP SOCKET STUFF==========
+	
+	tcp_client c;
+    string host;
+     
+    //cout<<"Enter hostname : ";
+    //cin>>host;
+     
+    //connect to host
+    //c.conn("10.0.0.3" , 8080); //When Ethernet is connected
+ 	c.conn("127.0.0.1" , 8080);	//When Ethernet is not connected (For isolated testing)
+     
+    //send some data
+    //c.send_data("C++ client for phone data connected!");	
+     
+    //receive and echo reply
+    //cout<<"BTReceive: Connected to TCP Server";
+    //cout<<c.receive(1024);
+    //cout<<"\n\n----------------------------\n\n";
+     
+    //done
+    //return 0;
+
+	//==============================================
 	bool connected = true;
 	
 	//Raspberry Pi's Bluetooth Address
@@ -99,29 +272,38 @@ int main(int argc, char **argv){
 
 	struct sockaddr_rc loc_addr = { 0 }, rem_addr = { 0 };
 	char buf[1024] = { 0 };
-	int bs, client, bytes_read;
+	int b = -1;
+	int client, bytes_read;
 	socklen_t opt = sizeof(rem_addr);
 
 	// allocate socket
-	bs = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+	b = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
 
 	// bind socket to port 1 of the first available 
 	// local bluetooth adapter
 	loc_addr.rc_family = AF_BLUETOOTH;
 	str2ba( address, &loc_addr.rc_bdaddr);
 	loc_addr.rc_channel = (uint8_t) 1;
-	bind(bs, (struct sockaddr *)&loc_addr, sizeof(loc_addr));
+	bind(b, (struct sockaddr *)&loc_addr, sizeof(loc_addr));
 	
 	// put socket into listening mode
-	listen(bs, 1);
-	printf("\nlistening...\n");
+	listen(b, 1);
+	printf("\nBTReceive: listening...\n\n");
+	//close(b);
 
 	// accept one connection
-	client = accept(bs, (struct sockaddr *)&rem_addr, &opt);
-	printf("\nconnection accepted!\n");
+	client = accept(b, (struct sockaddr *)&rem_addr, &opt);
+	if (client < 0){
+		perror("BTReceive: error in accept");
+		close(b);
+	}
+	else{
+		//printf(client);
+		printf("\nBTReceive: connection accepted!\n\n");
+	}
 
 	ba2str( &rem_addr.rc_bdaddr, buf );
-	fprintf(stderr, "accepted connection from %s\n", buf);
+	fprintf(stderr, "BTReceive: accepted connection from %s\n\n", buf);
 	memset(buf, 0, sizeof(buf));
 
 	while (connected){
@@ -129,10 +311,10 @@ int main(int argc, char **argv){
 		bytes_read = read(client, buf, sizeof(buf));
 		if(bytes_read == 1){
 			if (*buf != (char)9) {
-				int close = interpretCmd(buf);
+				int close = interpretCmd(buf, c);
 				if(close == 1){
 					connected = false;
-					printf("connection terminated\n");
+					printf("BTReceive: connection terminated\n\n");
 				}
 				//printf("received [%s]\n", buf);
 
@@ -149,7 +331,7 @@ int main(int argc, char **argv){
 
 	// close connection
 	close(client);
-	close(bs);
+	close(b);
 
 	return 0;
 }
