840223625bb574b6df22c528f48f5746e9899e36
Aaron <asgjass@gmail.com>
2018-04-23 15:48:50 -0400

    Intgrated hmi stuff with ROS

--- TouchscreenCode/Client.py
+++ TouchscreenCode/Client.py
@@ -1,30 +1,47 @@
+#!/usr/bin/env python
+
 import socket
 import sys
 
-py_ver = sys.version_info[0]
+import rospy
+from std_msgs.msg import int8
+
+#py_ver = sys.version_info[0]
+
+
+pubScreen = rospy.Publisher('screen_cmd', String, queue_size=10)
+pubPhone = rospy.Publisher('phone_cmd', String, queue_size=10)
+rospy.init_node('hmi_cmds', anonymous=True)
 
 def interpretCmd(data):
         if data == '0':
                 print('Client.py: Manual: Free Rolling Mode (0)\n')
-                #ROS stuff
+                #Publish 1 to screen_cmd topic
+                pubScreen.publish(1)
         elif data == '1':
                 print('Client.py: Manual: Brake Mode (1)\n')
-                #ROS stuff
+                #Publish 0 to screen_cmd topic
+                pubScreen.publish(0)
         elif data == '2':
                 print('Client.py: Autonomous: Cancel Command (2)\n')
-                #ROS stuff
+                #Publish 0 to phone_cmd topic
+                pubPhone.publish(0)
         elif data == '3':
                 print('Client.py: Autonomous: Go To Dock Command (3)\n')
-                #ROS stuff
+                #Publish 1 to phone_cmd topic
+                pubPhone.publish(1)
         elif data == '4':
                 print('Client.py: Autonomous: Park Command (4)\n')
-                #ROS stuff
+                #Publish 2 to phone_cmd topic
+                pubPhone.publish(2)
         elif data == '5':
                 print('Client.py: Autonomous: Extra Command (5)\n')
-                #ROS stuff
+                #Publish 3 to phone_cmd topic
+                pubPhone.publish(3)
         elif data == '6':
                 print('Client.py: Autonomous: Cancel Command (6)\n')
-                #ROS stuff
+                #Publish4 to phone_cmd topic
+                pubPhone.publish(4)
         elif data == '-1':
                 print('Client.py: Phone disconnected!\n')
         elif data == '-2':
@@ -47,7 +64,8 @@ while 1:
                 if not data:
                         print('Client.py: ERROR: Disconnected from TCP server!\n')
                         break
-                interpretCmd(data)
+                if not rospy.is_shutdown():
+                        interpretCmd(data)
 
         except Exception as e:
                 print(e)
